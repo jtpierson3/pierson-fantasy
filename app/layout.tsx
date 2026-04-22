f1f29d3c7379b53aa72f25cba063f73fa6f41d3c
jtpierson3 <jamespierson@Jamess-MBP.lan>
2026-04-22 18:53:55 -0500

    add clerk authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-//@ts-ignore
+//@ts-expect-error
 import "./globals.css";
 import { ClerkProvider } from '@clerk/nextjs'
 
