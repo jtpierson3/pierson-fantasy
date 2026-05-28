011dc7040cab8c10c8bca1ce7a55ca2d578bc2d5
jtpierson3 <jamespierson@Jamess-MBP.lan>
2026-05-27 19:19:57 -0500

    Type error fixed.

--- app/api/admin/survivor/stats/create/route.ts
+++ app/api/admin/survivor/stats/create/route.ts
@@ -21,7 +21,7 @@ export async function POST(req: Request) {
         select: { order: true }
       })
 
-      let order = (maxOrder?.order ?? 0) + 1
+      resolvedOrder = (maxOrder?.order ?? 0) + 1
     }
 
     await prisma.episodeStat.createMany({
