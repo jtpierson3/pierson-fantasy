52d083d626bf6d6478e66cd7cdc0dffcb6bf67b5
jtpierson3 <jamespierson@Jamess-MBP.lan>
2026-07-24 13:41:00 -0500

    Uncommenting dashboard link

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -10,10 +10,9 @@ type Props = {
 }
 
 const FOOTBALL_NAV = [
-    /* DASHBOARD
     {
         href: '/dashboard',
-        label: 'Dashboard',
+        label: 'Dashboard (In Progress)',
         icon: (
             <svg width="16" height="16" viewBox='0 0 16 16' fill="currentColor">
                 <rect x='1' y='1' width='6' height='6' rx='1' />
@@ -22,7 +21,7 @@ const FOOTBALL_NAV = [
                 <rect x='9' y='9' width='6' height='6' rx='1' />
             </svg>
         ),
-    },*/
+    },
         { href: '/dashboard/league',
         label: "League (Beta)",
         icon: (
